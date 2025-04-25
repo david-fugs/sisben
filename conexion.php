d61d123f38eb7fe44e4bac6d30c6778c6c160950
David <fulgore0142@gmail.com>
2025-04-25 14:36:15 -0500

    cambios db

--- conexion.php
+++ conexion.php
@@ -1,5 +1,7 @@
 <?php
-	
-	$mysqli = new mysqli("localhost", "aprendad_sisben", "~CY]&J9u#wxa", "aprendad_sisben");
-	
-?>
+
+$mysqli = new mysqli("localhost", "aprendad_sisben", "~CY]&J9u#wxa", "aprendad_sisben");
+
+//nueva db  BD: 	softepuc_sisben
+// USER: 	softepuc_sisben
+// PASS: 	)H7,OxCpo+W.*)a
